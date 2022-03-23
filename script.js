2a63094b6e8890adbca37b87732435fb8490d9eb
R Beith <rbeith@gmail.com>
2022-03-23 12:53:28 -0700

    Working on fixing the multiple operator problem.

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ const buttons = document.querySelectorAll('button');
 let output = document.querySelector('#output');
 
 let a = '';
-let b = ''
-let operator = '';
+let b = '';
+let operatorArr = [];
 let input = [];
+let operator = '';
 
-
-
-const findOperatorValue = function(input) {
-    if (input.includes('+')) {
-        return sliceValue = input.findIndex(operator => operator == '+')
-    } else if (input.includes('-')) {
-        return sliceValue = input.findIndex(operator => operator == '-')
-    } else if (input.includes('x')) {
-        return sliceValue = input.findIndex(operator => operator == 'x')
-    } else if (input.includes('÷')) {
-        return sliceValue = input.findIndex(operator => operator == '÷')
-    }
-};
+// const findOperatorValue = function(input) {
+//     if (input.includes('+')) {
+//         return sliceValue = input.findIndex(operator => operator == '+')
+//     } else if (input.includes('-')) {
+//         return sliceValue = input.findIndex(operator => operator == '-')
+//     } else if (input.includes('x')) {
+//         return sliceValue = input.findIndex(operator => operator == 'x')
+//     } else if (input.includes('÷')) {
+//         return sliceValue = input.findIndex(operator => operator == '÷')
+//     }
+// };
 
 const add = function(a, b) {
     return Math.round(a + b);
@@ -48,16 +47,20 @@ const operate = function(operator, a, b) {
     }
 }
 
+
+
+
 buttons.forEach((button) => {
         button.addEventListener('click', () => {
             if (button.id == '1' || button.id == '2' || button.id == '3' || button.id == '4'|| button.id == '5'|| button.id == '6' || button.id == '7' || button.id == '8' || button.id == '9' || button.id == '0') {
                 output.textContent += button.id;
-                input.push(button.id);
-                console.log(input)
+                a += button.id;
+                console.log(a)
             } else if (button.id == '+') {
                 output.textContent += button.id;
-                input.push(button.id);
-                operator = 'add';
+                operatorArr.push('add');
+                input.push(a)
+                a = '';
             } else if (button.id == '-') {
                 output.textContent += button.id;
                 input.push(button.id);
@@ -72,27 +75,35 @@ buttons.forEach((button) => {
                 operator = 'divide';
             } else if (button.id == '=') {
                 output.textContent += button.id;
-                findOperatorValue(input);
-                // sliceValue =
-                //     input.findIndex(operator => operator ===
-                //     ('+' || '-' || 'x' || '÷')
-                // );
-                console.log(findOperatorValue(input))
-                let a = input.slice(0, sliceValue).join('');
-                console.log(a)
+                input.push(a)
                 console.log(input)
-                // let c = input.slice(sliceValue).shift();
-                // console.log(c)
-                let b = input.slice(sliceValue + 1).join('');
-                console.log(b)
-                console.log(operator)
-                let result = operate(operator, Number(a), Number(b));
+                console.log(operatorArr)
+                let result = '';
+                for (i = input.length; i > 0; i--) {
+                    a = input.shift()
+                    console.log(a)
+                    operator = operatorArr.shift();
+                    console.log(operator)
+                    b = input.shift()
+                    console.log(b)
+                    operate(operator, Number(a), Number(b))
+                }
+                console.log(input)
+                    // findOperatorValue(input);
+                    // console.log(findOperatorValue(input))
+                    // let a = input.slice(0, sliceValue).join('');
+                    // console.log(a)
+                    // let b = input.slice(sliceValue + 1).join('');
+                    // console.log(b)
+                    // console.log(operator)
+                //let result = operate(operator, Number(a), Number(b));
                 output.textContent = result;
                 console.log(result)
+                a = '';
             } else if (button.id == 'C') {
                 input = [];
                 output.textContent = '';
-                operator = '';
+                operatorArr = [];
                 a = '';
                 b = '';
                 c = '';
